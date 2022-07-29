e74831c83fb6437386b5741c77bee9645a8a5c5a
Angel <86276165+Angelix1@users.noreply.github.com>
2022-07-29 22:13:41 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "350px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function botInvite() {
+  window.location.href = `https://discord.com/api/oauth2/authorize?client_id=827096460376932374&permissions=1632289287361&scope=bot%20applications.commands`;
+}
