6699c3cb9bf87c730a8370e7ff6340b717f3e778
Angel <86276165+Angelix1@users.noreply.github.com>
2022-08-17 20:04:15 +0800

    updated bot invite

--- script.js
+++ script.js
@@ -7,5 +7,5 @@ function closeNav() {
 }
 
 function botInvite() {
-  window.location.href = `https://discord.com/api/oauth2/authorize?client_id=827096460376932374&permissions=1632289287361&scope=bot%20applications.commands`;
+  window.location.href = `https://discord.com/api/oauth2/authorize?client_id=827096460376932374&permissions=1632826158289&scope=bot%20applications.commands`;
 }
