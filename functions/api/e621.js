ac2eab910519f75e995609c2d101b75e0d5ed4f2
Angel <86276165+Angelix1@users.noreply.github.com>
2022-09-08 13:46:01 +0800

    Update e621.js

--- functions/api/e621.js
+++ functions/api/e621.js
@@ -1,5 +1,6 @@
 export async function onRequestGet(req) {
   
   const res = await fetch(`https://api.angelwolf.workers.dev/api/${req}`);
-  return new Response(res);
+  let end = await res.json();
+  return new Response(end);
 }
